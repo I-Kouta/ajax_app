2a24e5576df150d7fb4b0571a7683fed497447f8
I-Kouta <kouta.baseball0222@gmail.com>
2022-01-18 16:06:20 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  # newアクションはコメントにした  
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index # ビューファイルを参照しない
   end
 end
